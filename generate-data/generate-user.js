de2ade67cc3af6ba98152e156e95c9ed59e8de42
Jeff Witters <jeff@Jeffs-MacBook-Pro.local>
2017-04-17 11:57:29 -0400

    Fake user generator update
    Added default password for all new users to be 000000
    Added admin Admin@healthcoin.com / 000000

--- generate-data/generate-user.js
+++ generate-data/generate-user.js
@@ -1,7 +1,6 @@
 
 const faker = require('faker');
 
-// Generates a user
 function generateUser() {
 	const gender = faker.random.number(1);
 	const firstname = faker.name.firstName(gender);
@@ -11,7 +10,8 @@ function generateUser() {
 
 	return {
 		email: faker.internet.exampleEmail(firstname, lastname),
-		password: faker.internet.password(faker.random.number({ min: 12, max: 16 })),
+		//password: faker.internet.password(faker.random.number({ min: 12, max: 16 })),
+		password: '$2a$10$TSS4ErE2nmuEfNXu.UrA6eBf0HiPhZhk5IegVhazrhOu6QwMZ46ee', // hash of 000000
 		firstname,
 		lastname,
 		gender: [ 'male', 'female' ][gender],
@@ -23,4 +23,29 @@ function generateUser() {
 	}
 }
 
-module.exports = generateUser;
+function generateAdmin() {
+	const gender = faker.random.number(1);
+	const firstname = faker.name.firstName(gender);
+	const lastname = faker.name.lastName();
+
+	const now = new Date();
+
+	return {
+		email: 'admin@healthcoin.com',
+		password: '$2a$10$TSS4ErE2nmuEfNXu.UrA6eBf0HiPhZhk5IegVhazrhOu6QwMZ46ee', // hash of 000000
+		firstname,
+		lastname,
+		gender: [ 'male', 'female' ][gender],
+		birthdate: faker.date.between('1937-01-01', '1999-01-01'),
+		created: now,
+		modified: now,
+		roles: ['admin'],
+		deleted: false,
+		_demo: true
+	}
+}
+
+module.exports = {
+	generateUser,
+	generateAdmin
+}
