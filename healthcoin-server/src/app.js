9b99acfb30373e14da772f7d37852edd032a03e5
James P. Javery <james.javery@gmail.com>
2017-04-25 13:38:45 -0500

    Minor fix

--- healthcoin-server/src/app.js
+++ healthcoin-server/src/app.js
@@ -27,20 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.configure(hooks());
 app.configure(rest());
 app.configure(services);
-app.use([
-	'/',
-	'/dashboard',
-	'/sign-in',
-	'/sign-up',
-	'/init',
-	'/coins',
-	'/bio-history',
-	'/admin/sign-in',
-	'/admin',
-	'/admin/groups',
-	'/admin/groups/:id',
-	'/404'
-], feathers.static(path.join(__dirname, config.get('public'), 'index.html')));
+app.use('/*', feathers.static(path.join(__dirname, config.get('public'), 'index.html')));
 app.configure(middleware);
 
 module.exports = app;
